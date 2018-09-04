765ca36178665935347d0389a17442bbb1293dfd
Paul Melero <paul.melero@erni-espana.es>
2018-09-04 14:21:21 +0200

    [xhr-start] solution

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -36,22 +36,22 @@ Instructions:
     /*
     This code needs to get wrapped in a Promise!
      */
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.onload = function() {
-      if (req.status === 200) {
-        // It worked!
-        // You'll want to resolve with the data from req.response
-      } else {
+    return new Promise((resolve, reject) => {
+      var req = new XMLHttpRequest();
+      req.open('GET', url);
+      req.onload = function() {
+        if (req.statusText.toLowerCase() === 'ok') {
+          resolve(req.response);
+        } else {
+          reject(req.statusText);
+        }
+      };
+      req.onerror = function() {
         // It failed :(
-        // Be nice and reject with req.statusText
-      }
-    };
-    req.onerror = function() {
-      // It failed :(
-      // Pass a 'Network Error' to reject
-    };
-    req.send();
+        reject('Network Error');
+      };
+      req.send();
+    })
   }
 
   window.addEventListener('WebComponentsReady', function() {
@@ -61,6 +61,15 @@ Instructions:
     You'll need to add a .then and a .catch. Pass the response to addSearchHeader on resolve or
     pass 'unknown' to addSearchHeader if it rejects.
      */
-    // get('../data/earth-like-results.json')
+    get('../data/earth-like-results.json')
+      .catch(err => {
+        console.log(err)
+        addSearchHeader('unknown')
+      })
+      .then(response => {
+        const res = JSON.parse(response)
+        addSearchHeader(res.query)
+        console.log({response: res})
+      })
   });
 })(document);
